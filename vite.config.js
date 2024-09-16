fcba7d0e0e073cca5b957e7bdb4553baa2751f32
yasaman <yasamanhn3@gmail.com>
2024-09-16 17:38:57 +0330

    edit again

--- vite.config.js
+++ vite.config.js
@@ -10,25 +10,19 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, "/src/pages/index.html"),
-        page1: path.resolve(__dirname, "/src/pages/blog.html"),
-        page2: path.resolve(__dirname, "/src/pages/contactWithUs.html"),
-        page3: path.resolve(__dirname, "/src/pages/inside-decoration.html"),
-        page4: path.resolve(__dirname, "/src/pages/javaaz.html"),
-        page5: path.resolve(__dirname, "/src/pages/outside-decoration.html"),
-        page6: path.resolve(__dirname, "/src/pages/projects.html"),
-        page7: path.resolve(__dirname, "/src/pages/ShowAllServices.html"),
-        page8: path.resolve(__dirname, "/src/pages/SingleBlog.html"),
-        page9: path.resolve(__dirname, "/src/pages/SingleDesignProject.html"),
-        page10: path.resolve(
-          __dirname,
-          "/src/pages/SingleServiceAnotherPage.html"
-        ),
-        page11: path.resolve(__dirname, "/src/pages/SingleServiceDesign.html"),
-        page12: path.resolve(
-          __dirname,
-          "/src/pages/SingleServiceNezaratr.html"
-        ),
+        main: "/src/pages/index.html",
+        page1: "/src/pages/blog.html",
+        page2: "/src/pages/contactWithUs.html",
+        page3: "/src/pages/inside-decoration.html",
+        page4: "/src/pages/javaaz.html",
+        page5: "/src/pages/outside-decoration.html",
+        page6: "/src/pages/projects.html",
+        page7: "/src/pages/ShowAllServices.html",
+        page8: "/src/pages/SingleBlog.html",
+        page9: "/src/pages/SingleDesignProject.html",
+        page10: "/src/pages/SingleServiceAnotherPage.html",
+        page11: "/src/pages/SingleServiceDesign.html",
+        page12: "/src/pages/SingleServiceNezaratr.html",
       },
     },
     assetsDir: "assets",
