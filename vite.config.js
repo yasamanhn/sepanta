f3e8d091c7a45934d7749e1eaa812940d75d885d
yasaman <yasamanhn3@gmail.com>
2024-09-16 15:56:09 +0330

    delete public directory

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,31 @@
 import { defineConfig } from "vite";
-import path from "path";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: "/pages/index.html",
-        page1: "/pages/blog.html",
-        page2: "/pages/contactWithUs.html",
-        page3: "/pages/inside-decoration.html",
-        page4: "/pages/javaaz.html",
-        page5: "/pages/outside-decoration.html",
-        page6: "/pages/projects.html",
-        page7: "/pages/ShowAllServices.html",
-        page8: "/pages/SingleBlog.html",
-        page9: "/pages/SingleDesignProject.html",
-        page10: "/pages/SingleServiceAnotherPage.html",
-        page11: "/pages/SingleServiceDesign.html",
-        page12: "/pages/SingleServiceNezaratr.html",
+        main: "/src/pages/index.html",
+        page1: "/src/pages/blog.html",
+        page2: "/src/pages/contactWithUs.html",
+        page3: "/src/pages/inside-decoration.html",
+        page4: "/src/pages/javaaz.html",
+        page5: "/src/pages/outside-decoration.html",
+        page6: "/src/pages/projects.html",
+        page7: "/src/pages/ShowAllServices.html",
+        page8: "/src/pages/SingleBlog.html",
+        page9: "/src/pages/SingleDesignProject.html",
+        page10: path.resolve(
+          __dirname,
+          "/src/pages/SingleServiceAnotherPage.html"
+        ),
+        page11: "/src/pages/SingleServiceDesign.html",
+        page12: path.resolve(
+          __dirname,
+          "/src/pages/SingleServiceNezaratr.html"
+        ),
       },
     },
   },
-  outDir: "dist",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
