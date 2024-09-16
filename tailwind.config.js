56f575869e55ae70fd85564b1c902b607aa1f475
yasaman <yasamanhn3@gmail.com>
2024-09-16 23:44:46 +0330

    edit directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./node_modules/flowbite/**/*.js", "./src/**/*.{html,js}"],
+  content: ["./node_modules/flowbite/**/*.js", "./**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
